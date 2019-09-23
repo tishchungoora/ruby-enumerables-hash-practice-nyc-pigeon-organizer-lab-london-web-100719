4dfdf55629268b5718b5cda5413095f6019f14c0
Tish Chungoora <tishal82@gmail.com>
2019-09-23 12:57:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,66 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  # Work with male and female pigeons to first construct a hash backbone by merging the male and female hashes
+  
+  male_pigeons = data[:gender][:male]
+  male_pigeon_list = {}
+  
+  female_pigeons = data[:gender][:female]
+  female_pigeon_list = {}
+  
+  male_pigeons.each do |name|
+    male_pigeon_list[name] = {:color => [], :gender => ["male"], :lives => []}
+  end
+
+  female_pigeons.each do |name|
+   female_pigeon_list[name] = {:color => [], :gender => ["female"], :lives => []}
+  end
+  
+  pigeon_list = male_pigeon_list.merge(female_pigeon_list)
+  
+  # Populate the applicable colors by visiting each pigeon key-value pair in the pigeon list. Find out if the pigeon name key is held as one of the values within any of the color key-value pairs. If so, convert the color key to a string and add it to pigeon list
+  color_of_pigeon = data[:color]
+  
+  pigeon_list.each do |name, attribute_hash|
+    color_of_pigeon.each do |color_sym, name_array|
+      if name_array.include?(name)
+        (pigeon_list[name][:color]).push(color_sym.to_s)
+      end
+    end
+  end
+  
+  # Populate the applicable locations by visiting each pigeon key-value pair in the pigeon list. Find out if the pigeon name key is held as one of the values within any of the location key-value pairs. If so, add it to pigeon list
+  residence_of_pigeon = data[:lives]
+  
+  pigeon_list.each do |name, attribute_hash|
+    residence_of_pigeon.each do |location, name_array|
+      if name_array.include?(name)
+        (pigeon_list[name][:lives]).push(location)
+      end
+    end
+  end
+  pigeon_list
 end
+
+
+# nyc_pigeon_organizer(pigeon_data)
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
